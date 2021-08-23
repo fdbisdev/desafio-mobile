51f841f7ca5143165d01c16655ad0e2267310365
Felipe Bis <felipe.bis@aluno.ifsp.edu.br>
2021-08-23 20:20:33 -0300

    refactor: change width for card component

--- src/components/MovieList/MovieCard/styles.ts
+++ src/components/MovieList/MovieCard/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   flex: 1;
   margin-bottom: 16px;
-  max-width: 150px;
+  max-width: 130px;
 `;
 
 export const MovieBanner = styled.ImageBackground`
